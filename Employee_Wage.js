f11fba4fc925b0bea50d6418df9eed220c47108a
Shreya Swaroop <shreyaswrp@gmail.com>
2020-09-10 21:56:16 +0530

    Added to calculate daily wages of employees

--- Employee_Wage.js
+++ Employee_Wage.js
@@ -1,7 +1,13 @@
 var msg = "Welcome to employee wage computation program";
 console.log(msg);
 
+let EMP_RATE_PER_HOUR = 20;
+let empHrs = 0;
+let empWage = 0;
 let isPresent = 1;
 let empCheck = Math.floor(Math.random() * 2);
-if (empCheck == 1) console.log("Employee is Present");
-else console.log("Employee is Absent");
+if (empCheck == 1) empHrs = 8;
+else empHrs = 0;
+
+empWage = empHrs * EMP_RATE_PER_HOUR;
+console.log(empWage);
