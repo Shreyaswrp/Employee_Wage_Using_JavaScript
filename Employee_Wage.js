bb71ddfaa29660f8fc12d651a2f6603ddb150220
Shreya Swaroop <shreyaswrp@gmail.com>
2020-09-10 21:46:41 +0530

    Added to check the attendance of employees

--- Employee_Wage.js
+++ Employee_Wage.js
@@ -1,2 +1,7 @@
 var msg = "Welcome to employee wage computation program";
 console.log(msg);
+
+let isPresent = 1;
+let empCheck = Math.floor(Math.random() * 2);
+if (empCheck == 1) console.log("Employee is Present");
+else console.log("Employee is Absent");
